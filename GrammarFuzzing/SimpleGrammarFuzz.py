63e1c6fd9b48b140d3f3e114a42fbf7ecfc1d5c0
Tyler Price <tylerprice@microsoft.com>
2022-10-06 21:07:49 -0400

    Add simple grammar fuzz

---
+++ GrammarFuzzing/SimpleGrammarFuzz.py
@@ -0,0 +1,88 @@
+import re
+import random
+from typing import List, Dict, Union, Any, Tuple, Optional
+
+Option = Dict[str, Any]
+Expansion = Union[str, Tuple[str, Option]]
+Grammar = Dict[str, List[Expansion]]
+
+EXPR_GRAMMAR: Grammar = {
+    "<start>":
+        ["<expr>"],
+
+    "<expr>":
+        ["<term> + <expr>", "<term> - <expr>", "<term>"],
+
+    "<term>":
+        ["<factor> * <term>", "<factor> / <term>", "<factor>"],
+
+    "<factor>":
+        ["+<factor>",
+         "-<factor>",
+         "(<expr>)",
+         "<integer>.<integer>",
+         "<integer>"],
+
+    "<integer>":
+        ["<digit><integer>", "<digit>"],
+
+    "<digit>":
+        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+}
+
+START_SYMBOL = "<start>"
+RE_NONTERMINAL = re.compile(r'(<[^<> ]*>)')
+
+class ExpansionError(Exception):
+    pass
+
+def nonterminals(expansion):
+    # In later chapters, we allow expansions to be tuples,
+    # with the expansion being the first element
+    if isinstance(expansion, tuple):
+        expansion = expansion[0]
+
+    return RE_NONTERMINAL.findall(expansion)
+
+def is_nonterminal(s):
+    return RE_NONTERMINAL.match(s)
+
+def simple_grammar_fuzzer(grammar: Grammar, 
+                          start_symbol: str = START_SYMBOL,
+                          max_nonterminals: int = 10,
+                          max_expansion_trials: int = 100,
+                          log: bool = False) -> str:
+    """Produce a string from `grammar`.
+       `start_symbol`: use a start symbol other than `<start>` (default).
+       `max_nonterminals`: the maximum number of nonterminals 
+         still left for expansion
+       `max_expansion_trials`: maximum # of attempts to produce a string
+       `log`: print expansion progress if True"""
+
+    term = start_symbol
+    expansion_trials = 0
+
+    while len(nonterminals(term)) > 0:
+        symbol_to_expand = random.choice(nonterminals(term))
+        expansions = grammar[symbol_to_expand]
+        expansion = random.choice(expansions)
+        # In later chapters, we allow expansions to be tuples,
+        # with the expansion being the first element
+        if isinstance(expansion, tuple):
+            expansion = expansion[0]
+
+        new_term = term.replace(symbol_to_expand, expansion, 1)
+
+        if len(nonterminals(new_term)) < max_nonterminals:
+            term = new_term
+            if log:
+                print("%-40s" % (symbol_to_expand + " -> " + expansion), term)
+            expansion_trials = 0
+        else:
+            expansion_trials += 1
+            if expansion_trials >= max_expansion_trials:
+                raise ExpansionError("Cannot expand " + repr(term))
+
+    return term
+
+print(simple_grammar_fuzzer(grammar=EXPR_GRAMMAR, max_nonterminals=5, log=False))
